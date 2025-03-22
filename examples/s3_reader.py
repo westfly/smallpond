7eb72d2891b88be1647cfeebc167ebb41e44d76f
aresyang <yweiyun@163.com>
2025-03-22 04:40:35 +0000

    add s3 ls support and param within repeated

--- examples/s3_reader.py
+++ examples/s3_reader.py
@@ -15,18 +15,79 @@ def parse_args():
         description="Read Parquet files from S3 using smallpond"
     )
     parser.add_argument(
-        "--s3-path",
+        "--s3-filename",
         type=str,
         default="s3://your-bucket/path/to/data.parquet",
         help="S3 path to Parquet file(s)",
     )
+    parser.add_argument(
+        "--s3-dir-path",
+        type=str,
+        default="",
+        help="S3 path to Parquet file(s)",
+    )
+    parser.add_argument(
+        "--region", type=str, default=None, help="AWS region (e.g., us-west-2)"
+    )
+    parser.add_argument("--number", default=10, type=int)
+    parser.add_argument("--repeat", default=3, type=int)
+    parser.add_argument(
+        "--endpoint",
+        type=str,
+        default=None,
+        help="Custom S3 endpoint for non-AWS S3-compatible storage",
+    )
+    parser.add_argument(
+        "--recursive", action="store_true", help="Recursively search for Parquet files"
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+
     return parser.parse_args()
+
+
+def get_file_list(args):
+    def parse_s3_url(s3_url):
+        from urllib.parse import urlparse
+
+        r = urlparse(s3_url, allow_fragments=False)
+        if r.scheme not in ("s3", "s3a"):
+            message = "invalid s3 url: %r" % (s3_url,)
+            raise ValueError(message)
+        path = r.path.lstrip("/")
+        return r.netloc, path
+
+    def parse_s3_dir_url(s3_url):
+        bucket, path = parse_s3_url(s3_url)
+        if not path.endswith("/"):
+            path += "/"
+        return bucket, path
+
+    import boto3
+
+    if not args.s3_dir_path:
+        return [args.s3_filename]
+
+    bucket, path = parse_s3_dir_url(args.s3_dir_path)
+    s3_client = boto3.client("s3", endpoint_url=args.endpoint, region_name=args.region)
+    objects = s3_client.list_objects_v2(
+        Bucket=bucket,
+        Prefix=f"{path}",
+    )
+    if "Contents" not in objects:
+        return []
+    filelist = []
+    for obj in objects["Contents"]:
+        print(obj)
+        file = obj["Key"]
+        if file.endswith("parquet"):
+            filelist.append(f"s3://{bucket}/{file}")
+    return filelist
 
 
 def read_file_check(args):
     dataset = ParquetDataSet(["tests/data/arrow/*.parquet"])
     print(f"{dataset.num_rows}")
-    dataset = ParquetDataSet([args.s3_path])
+    dataset = ParquetDataSet(get_file_list(args))
     print(f"{dataset.num_rows}")
 
 
@@ -79,10 +140,12 @@ def benchmark_read_file(args):
     import timeit
 
     conn = load_credentials_into_duckdb(args)
-    dataset_path = [args.s3_path]
+    dataset_path = get_file_list(args)
     dataset = ParquetDataSet(dataset_path)
     times = timeit.repeat(
-        lambda: read_from_dataset(dataset_path, dataset, conn), repeat=3, number=10
+        lambda: read_from_dataset(dataset_path, dataset, conn),
+        repeat=args.repeat,
+        number=args.number,
     )
     print("Time: {}".format(times))
 
